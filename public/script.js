16dfaa7d8cbd2fe4fb517f00868c8b066c018992
katnoriavipul138-source <katnoriavipul138@gmail.com>
2026-01-23 13:02:36 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -140,26 +140,3 @@ function addSystemMessage(text) {
 }
 
 
-
-
-// 🌙 Dark mode toggle
-const toggleBtn = document.getElementById("theme-toggle");
-
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-
-  // icon change
-  if (document.body.classList.contains("dark")) {
-    toggleBtn.textContent = "☀️";
-    localStorage.setItem("theme", "dark");
-  } else {
-    toggleBtn.textContent = "🌙";
-    localStorage.setItem("theme", "light");
-  }
-});
-
-// 🌙 Load saved theme
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark");
-  toggleBtn.textContent = "☀️";
-}
